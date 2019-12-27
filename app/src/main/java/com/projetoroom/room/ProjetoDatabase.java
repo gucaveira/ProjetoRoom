6e6e47c458200030388ce8182a198e5b9d8048e4
gustavo <pereiragustavo251>
2019-12-27 09:13:59 -0300

    migration com versionamento de db, recomendado

--- app/src/main/java/com/projetoroom/room/ProjetoDatabase.java
+++ app/src/main/java/com/projetoroom/room/ProjetoDatabase.java
@@ -2,9 +2,12 @@ package com.projetoroom.room;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.projetoroom.model.Aluno;
 import com.projetoroom.room.dao.AlunoDao;
@@ -19,7 +22,12 @@ public abstract class ProjetoDatabase extends RoomDatabase {
     public static ProjetoDatabase getInstance(Context context) {
         return Room.databaseBuilder(context, ProjetoDatabase.class, AGENDA_DB)
                 .allowMainThreadQueries()
-                .fallbackToDestructiveMigration()
+                .addMigrations(new Migration(1, 2) {
+                    @Override
+                    public void migrate(@NonNull SupportSQLiteDatabase database) {
+                        database.execSQL("ALTER TABLE aluno ADD COLUMN sobrenome TEXT");
+                    }
+                })
                 .build();
     }
 }
